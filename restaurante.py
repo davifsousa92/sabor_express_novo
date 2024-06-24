414ca4e709263f6ca5347f6132ff65ec159f5ec8
Davi Ferreira <davi.fsousa92@hotmail.com>
2024-06-24 16:21:39 -0300

    aula03_metodos de classes

--- restaurante.py
+++ restaurante.py
@@ -10,7 +10,8 @@ class Restaurante:
     def __str__(self):
         return f'{self._nome} | {self.categoria}'
     
-    def listar_restaurante():
+    @classmethod
+    def listar_restaurante(cls):
         print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         for restaurante in Restaurante.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
@@ -18,9 +19,13 @@ class Restaurante:
     @property
     def ativo(self):
         return '⌧' if self._ativo else '☐'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('praça','Gourmet')
 restaurante_praca._nome = 'Praça 2.0'
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('pizza Express','Italiana')
 
 Restaurante.listar_restaurante()
